0dd81ff874a3cff49df588e2e53a185fb15506ee
Anton Voronin <38187405+stalker8610@users.noreply.github.com>
2022-09-12 19:25:09 +0300

    1

--- api/profile/profile.js
+++ api/profile/profile.js
@@ -19,44 +19,141 @@ const prepareApiRequest = (apiUrl, accessToken) => {
 }
 
 
-function getProfileData(membership_id, accessToken, callback) {
+function getProfileData(membershipId, accessToken, callback) {
 
-    const reqOptions = prepareApiRequest(`/User/GetBungieNetUserById/${membership_id}/`, accessToken);
+    const reqOptions = prepareApiRequest(`/User/GetBungieNetUserById/${membershipId}/`, accessToken);
 
     fetch(reqOptions.url, { headers: reqOptions.headers })
         .then(
             (response) => {
-		if (response.status == 401) reject();
-		else {
-			//console.log(response);
-			return response.json();
-		}
-	    },
+                if (response.status == 401) reject();
+                else {
+                    //console.log(response);
+                    return response.json();
+                }
+            },
             (err) => {
                 console.log(`Error occured while get request to bungie.net:`)
-                console.log(`  membership_id = ${membership_id}`)
+                console.log(`  membership_id = ${membershipId}`)
                 console.log(`  request = ${reqOptions}`)
                 console.log(`  error = ${err}`);
                 reject();
             })
-        .then( json => callback(json), () => callback(null, 401) );
+        .then(json => callback(json), () => callback(null, 401));
+
+}
+
+async function getGamePlatformMembershipId(membershipId, accessToken, callback) {
+
+    const membershipType = 254; //BungieNext
+    const reqOptions = prepareApiRequest(`/User/GetMembershipsById/${membershipId}/${membershipType}/`, accessToken);
+
+    let membershipInfo;
+    let membershipsResponseJSON;
+
+    try {
+        membershipsResponseJSON = await fetch(reqOptions.url, { headers: reqOptions.headers })
+            .then(
+                (response) => {
+                    if (response.status == 401) reject(); //invalid token
+                    else {
+                        //console.log(response);
+                        return response.json();
+                    }
+                },
+                (err) => {
+                    console.log(`Error occured while get request to bungie.net:`)
+                    console.log(`  membership_id = ${membershipId}`)
+                    console.log(`  request = ${reqOptions}`)
+                    console.log(`  error = ${err}`);
+                    reject();
+                })
+
+    }
+    catch (err) {
+        console.log(err);
+    }
+
+    if (membershipsResponseJSON){
+
+        for(let membership in membershipsResponseJSON.Response.destinyMemberships){
+            membershipInfo.membershipType = membership.membershipType;
+            membershipInfo.membershipId = membership.membershipId
+            break;
+        }
+
+    }
+     
+    return membershipInfo;
 
 }
 
 
+function getCharactersData(membershipId, accessToken, callback) {
+
+    //first we have to know Steam / Epic store membership ID for further requests
+    let membershipData = getGamePlatformMembershipId(membershipId, accessToken, callback)
+
+    if (!membershipData) {
+        callback(null);
+        return;
+    }
+
+    const membershipType = 254; //BungieNet
+
+    const reqOptions = prepareApiRequest(`/Destiny2/${membershipData.membershipType}/Profile/${membershipData.membershipId}/
+                                            ?components=Profiles,Characters,CharacterProgressions`, accessToken);
+
+    fetch(reqOptions.url, { headers: reqOptions.headers })
+        .then(
+            (response) => {
+                if (response.status == 401) reject();
+                else {
+                    //console.log(response);
+                    return response.json();
+                }
+            },
+            (err) => {
+                console.log(`Error occured while get request to bungie.net:`)
+                console.log(`  membership_id = ${membershipId}`)
+                console.log(`  request = ${reqOptions}`)
+                console.log(`  error = ${err}`);
+                reject();
+            })
+        .then(json => callback(json), () => callback(null, 401));
+
+}
+
 let profileRouter = express();
 
-profileRouter.get('/', (req, res)=>{
+profileRouter.get('/', (req, res) => {
 
-    if (!req.session || !req.session.token || (req.session.token_expired_at < new Date())){
+    if (!req.session || !req.session.token || (req.session.token_expired_at < new Date())) {
         res.status(401).json(null);
-    }else{
-        getProfileData(req.session.membership_id, req.session.token, (userData, err)=>{
+    } else {
+        getProfileData(req.session.membership_id, req.session.token, (userData, err) => {
             if (err) res.status(401).json(null);
-            else{
+            else {
                 console.log(userData);
                 res.status(200).json(userData.Response);
-            } 
+            }
+        })
+    }
+
+})
+
+profileRouter.get('/characters', (req, res) => {
+
+    if (!req.session.membership_id) {
+        res.status(401).json(null);
+    } else {
+
+        getCharactersData(req.session.membership_id, req.session.token, (userData, err) => {
+            if (err) res.status(401).json(null);
+            else {
+                console.log(userData);
+                res.status(200).json(userData.Response);
+            }
         })
     }
 
