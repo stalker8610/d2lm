30d8b7aaa307835908548e4d5d81fdd2cc253b7c
Anton Voronin <38187405+stalker8610@users.noreply.github.com>
2022-09-15 12:40:08 +0300

    1

--- api/manifest/manifest.js
+++ api/manifest/manifest.js
@@ -4,12 +4,13 @@ const { spawn } = require('node:child_process');
 const https = require('https');
 const fs = require('fs');
 const { resolve } = require('node:path');
+const { MongoClient } = require('mongodb');
 
 //const BUNGIE_API_KEY = 'ed47e3f48b054bd5a323af81c1990a78'
 
 var collectionsManifest = new Map();
 
-async function readManifestCollections() {
+async function readManifestCollections(savedVersions) {
 
     console.log('Fetching manifest...');
     let json = await fetch('https://www.bungie.net/Platform/Destiny2/Manifest')
@@ -18,17 +19,22 @@ async function readManifestCollections() {
     console.log('Manifest fetched.');
 
 
- let count = 0;
+    let count = 0;
 
-    ['ru'].forEach( (language)=>{ 
+    ['ru'].forEach((language) => {
 
         console.log('Loading paths for language = ', language);
 
         for (let collection in json.Response.jsonWorldComponentContentPaths[language]) {
-            console.log('   ---- ', collection);
-            collectionsManifest.set(collection, json.Response.jsonWorldComponentContentPaths[language][collection]);
-	    count++;
-	    if (count>2) break;
+
+            if (savedVersions.get(collection) === json.Response.jsonWorldComponentContentPaths[language][collection]) {
+                console.log('   ---- ', collection, '\x1b[32m%s\x1b[0m', ' is UP TO DATE');
+            } else {
+                console.log('   ---- ', collection, '\x1b[33m%s\x1b[0m', ' NEED UPDATE');
+                collectionsManifest.set(collection, json.Response.jsonWorldComponentContentPaths[language][collection]);
+                count++;
+                if (count > 2) break;
+            }
         }
 
         console.log(`Done...`)
@@ -40,11 +46,9 @@ async function readManifestCollections() {
 async function downloadManifestCollectionsData() {
 
     for ([key, value] of collectionsManifest) {
-
         console.log(`Downloading collection ${key}...`);
-        //await downloadCollection(key, value);
-        //await mongoImport(key);
-
+        await downloadCollection(key, value);
+        await mongoImport(key);
     };
 
 }
@@ -52,8 +56,7 @@ async function downloadManifestCollectionsData() {
 
 function downloadCollection(collectionName, pathTo) {
 
-    let fileName = path.join(__dirname, 'downloads', collectionName+'.json');
-
+    let fileName = path.join(__dirname, 'downloads', collectionName + '.json');
 
     const file = fs.createWriteStream(fileName);
     return new Promise((resolve, reject) => {
@@ -76,8 +79,8 @@ function downloadCollection(collectionName, pathTo) {
 function mongoImport(collectionName) {
 
     console.log(`Move data into database...`);
-    let fileName = path.join(__dirname, 'downloads', collectionName+'.json');
-    //const command = `mongoimport --uri mongodb://admin:abcd@127.0.0.1:27017/d2lm?authSource=admin --collection ${collectionName} --type json --file ${fileName}`;
+    let fileName = path.join(__dirname, 'downloads', collectionName + '.json');
+    //const command = `mongoimport --uri mongodb://admin:abcd@127.0.0.1:27017/d2lm?authSource=admin --collection ${collectionName} --type json --file ${fileName} --drop`;
 
     return new Promise(() => {
 
@@ -91,7 +94,7 @@ function mongoImport(collectionName) {
                 'json',
                 '--file',
                 fileName,
-		'--drop'
+                '--drop'
             ]);
 
         cmd.stdout.on('data', (data) => {
@@ -109,17 +112,57 @@ function mongoImport(collectionName) {
     });
 }
 
+async function saveDownloadedVersionsInDatabase() {
+
+    const client = new MongoClient('mongodb://admin:abcd@127.0.0.1:27017/d2lm?authSource=admin');
+    await client.connect();
+
+    const collection = client.db('d2lm').collection('savedManifestVersions');
+
+    for ([key, value] of collectionsManifest) {
+
+        filter = { componentName: key }
+
+        const updateDoc = {
+            $set: {
+                pathTo: value
+            },
+        };
+
+        await collection.updateOne(filter, updateDoc, { upsert: true });
+        console.log('Version of ',key,' saved in db');
+    }
+
+    client.close();
+}
+
+async function readSavedVersions() {
+
+    console.log('Saved versions of components:');
+  
+    let savedVersions = new Map();
+
+    const client = new MongoClient('mongodb://admin:abcd@127.0.0.1:27017/d2lm?authSource=admin');
+    await client.connect();
+
+    const collection = client.db('d2lm').collection('savedManifestVersions');
+    await collection.find().forEach((doc) => {
+        savedVersions.set(doc.componentName, doc.pathTo);
+        console.log('   --- ', doc.componentName, '  ', doc.pathTo);
+    });
+
+    client.close();
+
+    return savedVersions;
+
+}
 
 (async () => {
 
-    await readManifestCollections();
+    const savedVersions = await readSavedVersions();
+    await readManifestCollections(savedVersions);
     await downloadManifestCollectionsData();
-
+    await saveDownloadedVersionsInDatabase();
     console.log('Operation done');
-  
+
 })()
-
-
-
-
-// module.exports = manifestRouter
