ed6b96876194f9f0aa09785b31196fd5b4c8610d
Anton Voronin <38187405+stalker8610@users.noreply.github.com>
2022-12-05 20:40:44 +0300

    1

--- api/auth/auth.js
+++ api/auth/auth.js
@@ -73,9 +73,8 @@ function isAuthorized(req){
 }
 
 function  checkAuth(req, res, next){
-    console.log('check auth insert')
     if (!isAuthorized(req)) {
-        console.log('checkAuth failed, send 401 Not authorized')
+        console.log('checkAuth failed, sent 401 Not authorized')
         res.status(401).json(null);
     }else{
         next();
